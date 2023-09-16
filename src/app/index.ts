393d4642e4e095e9fc03f95c44b2306a4e6c8851
Pranav Wani <pranavanilkumar.wani@hcl.com>
2023-09-16 15:16:24 +0530

    authentication added

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,13 @@
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import express, { json } from 'express'
+import cors from 'cors'
 import { User } from "./user";
 
 export async function initServer() {
     const app = express()
+
+    app.use(cors())
 
     const server = new ApolloServer({
         typeDefs: `
