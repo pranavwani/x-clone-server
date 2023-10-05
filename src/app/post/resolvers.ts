5463fc4b22b73bd353032df425bb9b035ba6aa91
Pranav Wani <pranavanilkumar.wani@hcl.com>
2023-10-05 18:08:20 +0530

    fix generating signed URL

--- src/app/post/resolvers.ts
+++ src/app/post/resolvers.ts
@@ -8,7 +8,7 @@ import UserService from "../../services/user";
 const UNAUTHENTICATED = "Unauthenticated"
 
 const s3Client = new S3Client({
-    region: process.env.AWS_S3_REGION
+    region: process.env.AWS_DEFAULT_REGION
 })
 
 const queries = {
